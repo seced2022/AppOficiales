647bba7ffc56dfd73b5f5caddfc23474c12ff22c
SECeD <114856253+seced2022@users.noreply.github.com>
2026-06-12 00:38:34 +0200

    firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,20 +1,23 @@
-// Configuración Firebase
-// 1. Crea un proyecto en Firebase.
-// 2. Activa Authentication y Firestore Database.
-// 3. Sustituye estos datos por los de tu proyecto.
+```javascript
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
+
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-auth.js";
+
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_PROYECTO.firebaseapp.com",
-  projectId: "TU_PROYECTO",
-  storageBucket: "TU_PROYECTO.appspot.com",
-  messagingSenderId: "TU_SENDER_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyATxYXL4Qvh8FYOso2L5sHIPg4Xx2EZpQs",
+  authDomain: "appoficiales-8acbd.firebaseapp.com",
+  projectId: "appoficiales-8acbd",
+  storageBucket: "appoficiales-8acbd.firebasestorage.app",
+  messagingSenderId: "483230341029",
+  appId: "1:483230341029:web:c9a4eecbf31946e3f9ba4c"
 };
 
-// Más adelante conectaremos aquí:
-// - Registro/login de usuarios
-// - Base de datos de oficiales
-// - Pruebas
-// - Convocatorias
-// - Aceptaciones/rechazos
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
+```
